fc16cfe3463b5aea21744edb3f85110c9c7ea5f9
Jon Dodds <jondodds@fmail.co.uk>
2021-08-23 12:20:52 +0100

    fix tabs

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,24 @@ fn main() {
     let days = ["first", "second", "third", "forth", "fifth", "sixth", 
 	            "seventh", "eighth", "ninth", "tenth", "eleventh", "twelth"];
 		
-	let gift_list = ["a partridge in a pear tree", "Two turtle doves", 
+    let gift_list = ["a partridge in a pear tree", "Two turtle doves", 
 	                 "Three french hens", "Four calling birds", "Five golden rings", 
 					 "Six geese a-laying", "Seven swans a-swimming", "Eight maids a-milking", 
 					 "Nine ladies dancing", "Ten lords a-leaping", "Eleven pipers piping",
 					 "Twelve drummers drumming"];
 
-	let mut counter = 0;
+    let mut counter = 0;
 	
-	for day in days {
+    for day in days {
 		
-		println!("On the {} of Christmas my true love gave to me", day);
+        println!("On the {} of Christmas my true love gave to me", day);
 				
-		if counter == 0 {
+	    if counter == 0 {
 			println!("{}", gift_list[0]);
 			counter = counter + 2;
 		} else {
-			let gift_slice = &gift_list[1..counter];  //partridge
+			
+			let gift_slice = &gift_list[1..counter];
 			
 			for gift in gift_slice.iter().rev() {
 				println!("{}", gift);	
@@ -28,6 +29,6 @@ fn main() {
 			println!("and {}", gift_list[0]);
 			counter = counter + 1;
 		}
-	
 	}
+	
 }
